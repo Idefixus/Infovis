57b8d50bf58552fbb61291aaa5474c1441130f5c
Robin Würz <robinwuerz@web.de>
2018-12-18 21:11:16 +0100

    Did a lot of work and added Maassen event

--- index.js
+++ index.js
@@ -15,14 +15,32 @@ var connection = mysql.createConnection({
     database: 'newspapers'
 });
 
+// Load maaßen content
+// LOAD JSON
+var fs = require("fs");
+console.log("\n *STARTING* \n");
+// Get content from file
+var contents = fs.readFileSync("views/maaßen.json");
+// Define to JSON type
+var jsonContent = JSON.parse(contents);
+// Get Value from JSON
+console.log("Name:", jsonContent.Name);
+console.log("Bio:", jsonContent.Bio);
+console.log("Ereignisse:", jsonContent.Ereignisse);
+console.log("\n *EXIT* \n");
+
+
 
 var publicDir = path.join(__dirname + '/res');
 app.use(express.static(publicDir));
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.set('view engine', 'html');
+
+// Get random events from db:
+// Load them with the start page by passing data on get
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/app.html'));
-  
+  res.render('app', {data: jsonContent});
 }); 
 app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname + '/select.html'));
@@ -57,6 +75,37 @@ app.post('/summary', function (req, res) {
         res.render('summary', { event: eventName, sum: sum, results: result });
     });
     //connection.end();
+});
+
+app.post('/maassen', function(req, res){
+
+    // DB 
+    // connect to the database: Throw error if not successful
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+    });
+
+    connection.query('SELECT *, MONTH(date), count(newsportal) AS count FROM newspapers.documents WHERE description LIKE "% Maaßen %" and MONTH(date)=9 GROUP BY newsportal ORDER BY count DESC;', function (err, result) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        
+        console.log(result);
+
+        var sum = 0;
+        Object.keys(result).forEach(function (key) {
+            var row = result[key];
+            sum += row.count;
+            }); 
+            
+            res.render('maassen', { data: jsonContent, sum: sum, results: result });
+    }); 
+        
+
 });
 
 
