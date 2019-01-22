17fefea592d1d9b7b63875bd102cb0b6115840cf
Alexandra <a.schwaighofer@gmx.de>
2019-01-23 00:10:07 +0100

    Preload data for quicker navigation; event button changed

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Folder for the infovis project
 
 Setup for testing:
 
-Pull master
-Npm install in Infovis folder
-DB User anlegen: user: test, pw: test
-Ihr müsst die relevamten Tabellen indizieren da ihr sonst ladezeiten von 1min habt und verbindungsabbrpche.
-Oder eigenen user anlegen wir haben keije Verönderungnan der DB die euch sonst betreffe
+- Pull master
+- npm install in Infovis folder
+- DB User anlegen: user: test, pw: test
+- Ihr müsst die relevanten Tabellen indizieren, da ihr sonst lange Ladezeiten habt und Verbindungsabbrüche.
+Oder eigenen user anlegen wir haben keine Veränderungen an der DB, die euch sonst betreffen
 
 ![alt text](https://github.com/Idefixus/Infovis/blob/master/res/preview1.PNG)
 
--- index.js
+++ index.js
@@ -24,10 +24,10 @@ var contents = fs.readFileSync("views/maaßen.json");
 // Define to JSON type
 var jsonContent = JSON.parse(contents);
 // Get Value from JSON
-console.log("Name:", jsonContent.Name);
-console.log("Bio:", jsonContent.Bio);
-console.log("Ereignisse:", jsonContent.Ereignisse);
-console.log("\n *EXIT* \n");
+//console.log("Name:", jsonContent.Name);
+//console.log("Bio:", jsonContent.Bio);
+//console.log("Ereignisse:", jsonContent.Ereignisse);
+//console.log("\n *EXIT* \n");
 
 //----Wiesn-JSON-Action------
 var contentsWiesn = fs.readFileSync("views/oktoberfest.json");
@@ -37,58 +37,133 @@ var jsonContentWiesn = JSON.parse(contentsWiesn);
 var contentsRegierung = fs.readFileSync("views/bundesregierung.json");
 var jsonContentRegierung = JSON.parse(contentsRegierung);
 
-//----Test
+//----load data for German cities ----
 var contentGermanCities = fs.readFileSync("res/datapublishers.json");
 var jsonGermanCities = JSON.parse(contentGermanCities);
 
+var finalcityarray = []
+var finalCityStats = {}
 
+// preload data from database for single cities
+var calcCityStats = function (callback) {
 
-var publicDir = path.join(__dirname + '/res');
-app.use(express.static(publicDir));
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.set('view engine', 'html');
-
-
-// Connect to the database.
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-});
-var jsonData;
-app.get('/', function (req, res) {
-    console.log(JSON.stringify(jsonGermanCities, null, 2));
     var jsonLength = Object.keys(jsonGermanCities.features).length;
+    var calc = function (city) {
 
-    // Get random events from db:
-    // Load them with the start page by passing data on get
+        connection.query(`Select round((b.amount/a.amount)*100, 2) as Amount, a.month as Year from
+        (select sum(amount) as amount, month from (
+        Select count(id) as amount, date_format(date, "%m") as month
+        from newspapers.documents 
+        where date between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('31/09/2018', '%d/%m/%Y') 
+        group by month
+        UNION
+        Select count(id) as amount, date_format(str_to_date(date, '%d.%m.%Y'), "%m") as month
+        from newspapers.documents 
+        where newsportal = "Bild" 
+        	and str_to_date(date, '%d.%m.%Y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+            group by month
+        UNION
+        Select count(id) as amount, date_format(str_to_date(date, '%a, %d %b %y'), "%m") as month
+        from newspapers.documents 
+        where newsportal = "Donaukurier" 
+            and str_to_date(date, '%a, %d %b %y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+            group by month
+        UNION
+        Select count(id) as amount, date_format(str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') , "%m") as month
+        from newspapers.documents 
+        where newsportal = "Sueddeutsche Zeitung" 
+            and str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') 
+	        between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+            group by month
+        ) as t
+        group by month 
+        order by month) a,
+        (select sum(amount) as amount, month from(
+         Select count(id) as amount, date_format(date, "%m") as month
+           from newspapers.documents 
+           where (description LIKE '%` + city + `%' or title like '%` + city + `%') and date between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('31/09/2018', '%d/%m/%Y') 
+           group by month
+         UNION
+        Select count(id) as amount, date_format(str_to_date(date, '%d.%m.%Y'), "%m") as month
+        from newspapers.documents 
+        where newsportal = "Bild" 
+        	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
+           	and str_to_date(date, '%d.%m.%Y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+            group by month
+        UNION
+        Select count(id) as amount, date_format(str_to_date(date, '%a, %d %b %y'), "%m") as month
+        from newspapers.documents 
+        where newsportal = "Donaukurier" 
+        	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
+            and str_to_date(date, '%a, %d %b %y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+        	group by month
+        UNION
+        Select count(id) as amount, date_format(str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') , "%m") as month
+        from newspapers.documents 
+        where newsportal = "Sueddeutsche Zeitung" 
+        	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
+            and str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') 
+        	between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
+            group by month
+           ) as t
+           group by month
+           order by month) b where b.month = a.month`, function (err, result) {
+               if (err) {
+                   console.error(err);
+                   return;
+               }
+               var resultarray = [];
+               var month = 1;
+               for (var i = 0; i < result.length; i++) {
+                   var row = result[i];
+                   while (parseInt(row.Year) != month) {
+                       var insertmonth = '0' + month;
+                       resultarray.push({ Amount: 0, Year: insertmonth });
+                       month++;
+                   }
+                   resultarray.push(row);
+                   month++;
+               }
 
-    var events = {"events": [
-                            {"name": "maassen", "image": "Maaßen.jpg"},
-                            {"name": "oktoberfest", "image": "oktoberfest.png"},
-                            {"name": "bundesregierung", "image": "bundestag.png"},
-                            {"name": "hamburg", "image": "icon_hamburg.png"}
-                        ] };
-    var number = Math.floor((Math.random() * 3));
-    var currentevent = events.events[number];
-    
-    switch (currentevent.name) {
-        case "maassen":
-            jsonData = jsonContent;
-            break;
-        case "oktoberfest":
-            jsonData = jsonContentWiesn;
-            break;
-        case "bundesregierung":
-            jsonData = jsonContentRegierung;
-            break;
+               console.log(resultarray);
+
+        
+               //TOP 5 keywords
+               connection.query(`SELECT keyword, COUNT(b.id) AS anzahl
+                FROM documents AS b
+                JOIN documents_keywords AS a ON a.document_id = b.id
+                JOIN keywords AS c ON a.keyword_id = c.id
+                WHERE (description LIKE '%` + city + `%' or title like '%` + city + `%') and keyword not in ('` + city + `', 'Deutschland', 'Politik', 'Sport', 'Regional', 'Fussball', '', 'Süddeutsche Zeitung München')
+                GROUP BY keyword
+               ORDER BY anzahl DESC
+               limit 5;`, function (err, result) {
+                   if (err) {
+                       console.error(err);
+                       return;
+                   }
+                   console.log(result);
+                   finalCityStats[city] = { monthdata: resultarray, top5: result };
+
+                   // this is the last successfull callback
+                   console.log(Object.keys(finalCityStats))
+                   if (Object.keys(finalCityStats).length == jsonLength) {
+                       callback()
+                   }
+
+               });
+
+            });
     }
-    console.log(currentevent);
-    console.log("Number:" + number);
-    console.log(jsonData);
+    for (var i = 0; i < jsonLength; i++) {
+        calc(jsonGermanCities.features[i].city);
+    }
 
+}
+
+// preload data from database for the Germany map
+var calcMainStats = function (callback) {
     var cityarray = [];
+    var jsonLength = Object.keys(jsonGermanCities.features).length;
     for (var i = 0; i < jsonLength; i++) {
         var city = jsonGermanCities.features[i].city;
         connection.query(`select sum(sum) as sum, city from (
@@ -119,122 +194,84 @@ app.get('/', function (req, res) {
                 console.error(err);
                 return;
             }
-            
             cityarray.push(result[0]);
             console.log(result[0]);
+
+            // this is the last successfull callback
             if (cityarray.length == jsonLength) {
-                res.render('index', { cityarray: cityarray, currentevent: currentevent, data: jsonData });
+                callback()
+                finalcityarray = cityarray
             }
         });
     }
+}
+
+// call preload functions and wait for callback to open port 
+calcMainStats(function () {
+    calcCityStats(function () {
+        app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+            console.log("NOW EVERTHINGS IS READY!")
+        });
+    })
+})
+
+
+var publicDir = path.join(__dirname + '/res');
+app.use(express.static(publicDir));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Connect to the database.
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+});
+
+var jsonData;
+
+app.get('/', function (req, res) {
+    console.log(JSON.stringify(jsonGermanCities, null, 2));
+    // Get random events from db:
+    // Load them with the start page by passing data on get
+
+    var events = {"events": [
+                            {"name": "maassen", "image": "Maaßen.jpg"},
+                            {"name": "oktoberfest", "image": "oktoberfest.png"},
+                            {"name": "bundesregierung", "image": "bundestag.png"},
+                            {"name": "hamburg", "image": "icon_hamburg.png"}
+                        ] };
+    var number = Math.floor((Math.random() * 3));
+    var currentevent = events.events[number];
+    
+    switch (currentevent.name) {
+        case "maassen":
+            jsonData = jsonContent;
+            break;
+        case "oktoberfest":
+            jsonData = jsonContentWiesn;
+            break;
+        case "bundesregierung":
+            jsonData = jsonContentRegierung;
+            break;
+    }
+    console.log(currentevent);
+    console.log("Number:" + number);
+    console.log(jsonData);
+    res.render('index', { cityarray: finalcityarray, currentevent: currentevent, data:jsonData });
 }); 
+
 
 app.get('/drawlinechart', function (req, res) {
     console.log("passed parameter: city =" + req.query.city);
     var city = req.query.city;
-    var resultarray = [];
-    
-    connection.query(`Select round((b.amount/a.amount)*100, 2) as Amount, a.month as Year from
-(select sum(amount) as amount, month from (
-Select count(id) as amount, date_format(date, "%m") as month
-from newspapers.documents 
-where date between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('31/09/2018', '%d/%m/%Y') 
-group by month
-UNION
-Select count(id) as amount, date_format(str_to_date(date, '%d.%m.%Y'), "%m") as month
-from newspapers.documents 
-where newsportal = "Bild" 
-	and str_to_date(date, '%d.%m.%Y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-    group by month
-UNION
-Select count(id) as amount, date_format(str_to_date(date, '%a, %d %b %y'), "%m") as month
-from newspapers.documents 
-where newsportal = "Donaukurier" 
-    and str_to_date(date, '%a, %d %b %y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-    group by month
-UNION
-Select count(id) as amount, date_format(str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') , "%m") as month
-from newspapers.documents 
-where newsportal = "Sueddeutsche Zeitung" 
-    and str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') 
-	between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-    group by month
-) as t
-group by month 
-order by month) a,
-(select sum(amount) as amount, month from(
- Select count(id) as amount, date_format(date, "%m") as month
-    from newspapers.documents 
-    where (description LIKE '%` + city + `%' or title like '%` + city + `%') and date between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('31/09/2018', '%d/%m/%Y') 
-   group by month
- UNION
-Select count(id) as amount, date_format(str_to_date(date, '%d.%m.%Y'), "%m") as month
-from newspapers.documents 
-where newsportal = "Bild" 
-	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
-	and str_to_date(date, '%d.%m.%Y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-    group by month
-UNION
-Select count(id) as amount, date_format(str_to_date(date, '%a, %d %b %y'), "%m") as month
-from newspapers.documents 
-where newsportal = "Donaukurier" 
-	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
-    and str_to_date(date, '%a, %d %b %y') between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-	group by month
-UNION
-Select count(id) as amount, date_format(str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') , "%m") as month
-from newspapers.documents 
-where newsportal = "Sueddeutsche Zeitung" 
-	and (description LIKE '%` + city + `%' or title like '%` + city + `%')  
-    and str_to_date(replace(replace(replace(replace(substring(date, 5, 25), 'Mär', 'Mar'), 'Dez', 'Dec'), 'Mai', 'May'), 'Okt', 'Oct'), '%d %b %Y') 
-	between str_to_date('01/01/2018', '%d/%m/%Y') and str_to_date('30/09/2018', '%d/%m/%Y')
-    group by month
-    ) as t
-    group by month
-    order by month) b where b.month = a.month`, function (err, result) {
-
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        var month = 1;
-        for (var i = 0; i < result.length; i++) {
-            var row = result[i];
-            while (parseInt(row.Year) != month) {
-                var insertmonth = '0' + month;
-                resultarray.push({ Amount: 0, Year: insertmonth });
-                month++;
-            }
-            resultarray.push(row);
-            month++;
-        }
-
-        console.log(resultarray);
-
-    });
-
-    //TOP 5 keywords
-    connection.query(`SELECT keyword, COUNT(b.id) AS anzahl
-    FROM documents AS b
-    JOIN documents_keywords AS a ON a.document_id = b.id
-    JOIN keywords AS c ON a.keyword_id = c.id
-    WHERE (description LIKE '%` + city + `%' or title like '%` + city + `%') and keyword not in ('` + city + `', 'Deutschland', 'Politik', 'Sport', 'Regional', 'Fussball', '', 'Süddeutsche Zeitung München')
-    GROUP BY keyword
-    ORDER BY anzahl DESC
-    limit 5;`, function (err, result) {
-        
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        console.log(result);
-        res.json({ monthdata: resultarray, top5: result });
-        });
+    res.json(finalCityStats[city]);
+  
 });
 
-// TODO
+
 app.post('/summary', function (req, res) {
     var passedUrl = req.url.split("=");
     var eventName = passedUrl[passedUrl.length - 1];
@@ -260,7 +297,7 @@ app.post('/summary', function (req, res) {
 
 });
 
-app.post('/maassen', function(req, res){
+app.get('/maassen', function(req, res){
 
     connection.query('SELECT *, MONTH(date), count(newsportal) AS count FROM newspapers.documents WHERE description LIKE "% Maaßen %" and MONTH(date)=9 GROUP BY newsportal ORDER BY count DESC;', function (err, result) {
         if (err) {
@@ -283,7 +320,7 @@ app.post('/maassen', function(req, res){
 });
 
 
-app.post('/oktoberfest', function (req, res) {
+app.get('/oktoberfest', function (req, res) {
 
     connection.query('SELECT *, ' /*MONTH(date),*/ + 'count(newsportal) AS count FROM newspapers.documents WHERE description LIKE "% Oktoberfest %"' /* and MONTH(date)=9*/ + ' GROUP BY newsportal ORDER BY count DESC;', function (err, result) {
         if (err) {
@@ -343,9 +380,5 @@ app.get('/bundesregierung', function (req, res) {
 
         res.render('bundesregierung', { data: jsonContentRegierung, sum: sum, results: result });
     });
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
 });
 
